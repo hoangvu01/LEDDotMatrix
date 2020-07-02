954bfdf80906ee263c0e10d12ed2600d5fee8d5c
HoangVu@ryzen-pc <vuhoang1609@gmail.com>
2020-07-02 18:17:58 +0100

    Refactored parser into individual types for int/str/bool

--- controller/front-end/matrix/src/homepage/DisplaySlider.tsx
+++ controller/front-end/matrix/src/homepage/DisplaySlider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Slider, Typography } from '@material-ui/core';
+import { Slider, Typography, Switch, FormControlLabel } from '@material-ui/core';
 import { Container, Row, Col, Button,
          Form, FormGroup, Label, Input } from 'reactstrap';
 
@@ -17,6 +17,9 @@ class DisplaySlider extends React.Component<{}, IState> {
       ledAttrs: {}
     };
     this.postLEDAttribute = this.postLEDAttribute.bind(this); 
+    this.processIntAttribute = this.processIntAttribute.bind(this);
+    this.processStrAttribute = this.processStrAttribute.bind(this);
+    this.processBoolAttribute = this.processBoolAttribute.bind(this);
   }
 
   componentDidMount() {
@@ -45,59 +48,91 @@ class DisplaySlider extends React.Component<{}, IState> {
       method: 'POST',
       headers: {'Content-Type' : 'Application/json' },
       body: JSON.stringify(ledAttrValues)
-    }).then(() => {window.location.reload()})
+    }).then(() => {window.location.reload()} )
     
+  }
+
+  processIntAttribute(key : string) {
+    const { ledAttrs } = this.state;
+    return ( 
+      <Col xs="8" md={{size : 4, offset: 4}}  id={key}>
+        <Typography id={key}> {key} </Typography>
+        <Slider
+          defaultValue = {ledAttrs[key]['value'] 
+                          ? ledAttrs[key]['value']
+                          : ledAttrs[key]['min']}
+          step = {1}
+          min = {ledAttrs[key]['min']}
+          max = {ledAttrs[key]['max']}
+          valueLabelDisplay = "auto"
+          aria-labelledby = "discrete-slider"
+          onChange = { (e, val) => {
+              var curLed = this.state.ledAttrs;
+              curLed[key]["value"] = val;
+              this.setState({ledAttrs : curLed});
+          }}
+        />   
+      </Col>  
+    );
+  } 
+  
+  processStrAttribute(key : string) {
+    return (
+      <FormGroup> 
+        <Label> {key} </Label>
+        <Input 
+          id={key} 
+          name={key} 
+          onChange={(event : any) => {
+            var curLed = this.state.ledAttrs;
+            curLed[key]["value"] = event.target.value;
+            this.setState({ledAttrs : curLed})
+          }}
+          placeholder="Type here..."/>
+      </FormGroup> 
+    );
+  }
+
+  processBoolAttribute(key : string) {
+    const { ledAttrs } = this.state;
+    return (
+      <Col xs="8" md={{size : 4, offset: 4}}  id={key}>
+        <FormControlLabel label={key} control={
+          <Switch
+            checked={ledAttrs[key]["value"]}
+            onChange={(event : any) => {
+              var curLed = this.state.ledAttrs;
+              curLed[key]["value"] = event.target.checked;
+              this.setState({ledAttrs : curLed})
+            }}
+           />}
+         />
+      </Col>
+    );
   }
  
   render() {
     const { loaded, ledAttrs } = this.state;
     var sliderIntArr : any[] = [];
     var sliderStrArr : any[] = [];
+    var sliderBoolArr : any[] = [];
     if (loaded) {
       for (let key in ledAttrs) {
-        console.log(ledAttrs[key]);
         if (ledAttrs[key]["type"] === 'int') {
-          sliderIntArr.push(
-            <Col xs="8" md={{size : 4, offset: 4}}  id={key}>
-              <Typography id={key}> {key} </Typography>
-              <Slider
-                defaultValue = {ledAttrs[key]['value'] 
-                                ? ledAttrs[key]['value']
-                                : ledAttrs[key]['min']}
-                step = {1}
-                min = {ledAttrs[key]['min']}
-                max = {ledAttrs[key]['max']}
-                valueLabelDisplay = "auto"
-                aria-labelledby = "discrete-slider"
-                onChange = { (e, val) => {
-                    var curLed = this.state.ledAttrs;
-                    curLed[key]["value"] = val;
-                    this.setState({ledAttrs : curLed});
-                }}
-              />   
-            </Col>  
-          );
+          sliderIntArr.push(this.processIntAttribute(key));
         } else if (ledAttrs[key]["type"] === 'str') {
-          sliderStrArr.push(
-            <FormGroup> 
-              <Label> {key} </Label>
-              <Input 
-                id={key} 
-                name={key} 
-                onChange={(event : any) => {
-                  var curLed = this.state.ledAttrs;
-                  curLed[key]["value"] = event.target.value;
-                  this.setState({ledAttrs : curLed})
-                }}
-                placeholder="Type here..."/>
-            </FormGroup> 
-          )
+          sliderStrArr.push(this.processStrAttribute(key));
+        } else if (ledAttrs[key]["type"] === 'bool') {
+          sliderBoolArr.push(this.processBoolAttribute(key));
         }
       }
     }
 
     return (
       <Container fluid>
+        <Row>
+          {sliderBoolArr}
+        </Row>
         <Row xs="8" md="6"> 
           {sliderIntArr} 
         </Row>
